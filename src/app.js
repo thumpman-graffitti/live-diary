6aa9d3ce1a887db0b43339c128e45265903b081c
thumpman-graffitti <thumpman_graffitti@yahoo.co.jp>
2026-01-25 12:36:33 +0900

    app.js を更新

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-
+window.addEventListener("DOMContentLoaded", async () => {
+  try {
+    await openDB();
+    initRegisterUI();
+    loadHistory();
+  } catch (e) {
+    alert("DBの初期化に失敗しました");
+    console.error(e);
+  }
+});
